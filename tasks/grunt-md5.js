855bd0c497500956376790e105650b29ec78e8f6
Jean-Sebastien Ney <jeansebastien.ney@gmail.com>
2013-02-06 14:23:05 +0100

    after after

--- tasks/grunt-md5.js
+++ tasks/grunt-md5.js
@@ -14,99 +14,91 @@ module.exports = function(grunt) {
   // TODO: ditch this when grunt v0.4 is released
   grunt.util = grunt.util || grunt.utils;
 
-  var _ = grunt.util._, path = require('path');
+  var _ = grunt.util._;
+  var path = require('path');
 
   grunt.registerMultiTask('md5', 'Generate a md5 filename', function() {
-    var options = this.options();
     var destDir;
-    var isExpandedPair;
+    // file object : {newPath: /***/, oldPath: /***/, content: /***/}
+    var currentFile;
+    var options = this.options();
 
     grunt.verbose.writeflags(options, 'Options');
 
-    // Keep track of processedFiles so we can call the `after` callback if needed.
-    var processedFiles = [];
-    this.files.forEach(function(filePair) {
-      // Changes for each file (old path, new path, file content)
-      var fileChange;
+    this.files.forEach(function(file) {
+      // Keep track of processedFiles so we can call the `after` callback if needed.
+      var processedFiles = [];
 
-      isExpandedPair = filePair.orig.expand || false;
-
-      grunt.verbose.writeln('Files: ' + filePair.src);
-      if (typeof filePair.src === 'undefined') {
-        grunt.fail.warn("Files object doesn't exist");
+      if (typeof file.src === 'undefined') {
+        grunt.fail.warn('Files object doesn\'t exist');
       }
 
-      filePair.src.forEach(function(srcFile) {
-        if (detectDestType(filePair.dest) === 'directory') {
-          destDir = (isExpandedPair) ? filePair.dest : unixifyPath(path.join(filePair.dest, srcFile));
-        } else {
-          destDir = filePair.dest;
-        }
+      if (!grunt.file.exists(file.dest)) {
+        grunt.file.mkdir(file.dest);
+        grunt.verbose.writeln('Directory \'' + file.dest + '\' created.');
+      }
 
-        grunt.verbose.writeln('Destination directory:' + destDir);
+      var srcFiles = grunt.file.expand(file.src);
+      var destDir = grunt.file.expand(file.dest)[0];
 
-        if (grunt.file.exists(destDir) === false) {
-          grunt.verbose.writeln("Creating destination directory as it didn't exist.");
-          grunt.file.mkdir(destDir);
-        }
+      grunt.verbose.writeln('Files: ' + file.src);
+      grunt.verbose.writeln('Destination directory: \'' + destDir + '\'');
 
+      srcFiles.forEach(function(srcFile) {
         try {
-          var srcCode = grunt.file.read(srcFile), ext = '', basename = '', filename, destFile;
+          var basename = '';
+          var destFile;
+          var ext = '';
+          var filename;
+          var srcCode = grunt.file.read(srcFile);
+
           // keep extension unless you explicitly tell to not
           if (options.keepExtension !== false) {
             ext = path.extname(srcFile);
           }
+
           // keep basename unless you explicitly tell to not
           if (options.keepBasename !== false) {
             basename = path.basename(srcFile, ext || path.extname(srcFile)) + '-';
           }
+
           filename = basename +
             require('crypto').
             createHash('md5').
             update(srcCode).
             digest('hex') + ext;
 
-          destFile = path.join(destDir, filename);
+          destFile = path.join(file.dest, filename);
 
           grunt.file.copy(srcFile, destFile);
 
-          fileChange = {
+          currentFile = {
             newPath: destFile,
             oldPath: srcFile,
             content: srcCode
           };
 
+          // for callback after each file
           if (_.isFunction(options.afterEach)) {
-            options.afterEach(fileChange);
+            options.afterEach(currentFile);
           }
-          processedFiles.push(fileChange);
-          grunt.log.writeln("File '" + destFile + "' created.");
+
+          if (_.isFunction(options.after)) {
+            processedFiles.push(currentFile);
+          }
+
+          grunt.log.writeln('File \'' + destFile + '\' created.');
         } catch(err) {
           grunt.log.error(err);
-          grunt.fail.warn("Fail to generate an MD5 file name");
+          grunt.fail.warn('Fail to generate an MD5 file name');
         }
       });
 
-      if (options.after) {
+      // call `after` if defined
+      if (_.isFunction(options.after)) {
         options.after(processedFiles);
       }
     });
   });
-
-  var detectDestType = function(dest) {
-    if (grunt.util._.endsWith(dest, '/')) {
-      return 'directory';
-    } else {
-      return 'file';
-    }
-  };
-
-  var unixifyPath = function(filepath) {
-    if (process.platform === 'win32') {
-      return filepath.replace(/\\/g, '/');
-    } else {
-      return filepath;
-    }
-  };
 };
 
